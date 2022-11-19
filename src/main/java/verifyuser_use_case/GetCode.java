5fa08f8904e5743351591697422fd93d258749d2
allenxu <allenx.03819@gmail.com>
2022-11-19 11:02:08 -0500

    implemented GenerateEmail class, getcode class, sendemail class, verify user class

--- src/main/java/verifyuser_use_case/GetCode.java
+++ src/main/java/verifyuser_use_case/GetCode.java
@@ -1,4 +1,33 @@
 package verifyuser_use_case;
 
+import com.mongodb.client.MongoIterable;
+import com.mongodb.client.model.Filters;
+import org.bson.Document;
+import org.bson.conversions.Bson;
+import org.bson.types.ObjectId;
+
+import java.util.Date;
+
 public class GetCode {
+    public static String getVerificationCode(ObjectId userID) {
+        Bson filter = Filters.eq("userID", userID);
+
+        MongoIterable<Document> results = connectionManager.getCollection("Verification").find(filter);
+
+        if (results.first() != null) {
+            Document verificationDoc = results.first();
+
+            Date currentTime = new Date();
+            Date createdTime = verificationDoc.getDate("createdTime");
+
+            if (currentTime.compareTo(AddMinutes.addMinutesToDate(5, createdTime)) > 0) {
+                connectionManager.getCollection("Verification").deleteMany(Filters.eq("userID", userID));
+                return "";
+            } else {
+                return verificationDoc.getString("code");
+            }
+        } else {
+            return "";
+        }
+    }
 }
