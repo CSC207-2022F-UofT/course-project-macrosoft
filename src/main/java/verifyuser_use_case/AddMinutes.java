713251edb77be5ad712411bf2b7de7373d89e404
allenxu <allenx.03819@gmail.com>
2022-11-19 10:16:57 -0500

    implemented addminutes class

--- src/main/java/verifyuser_use_case/AddMinutes.java
+++ src/main/java/verifyuser_use_case/AddMinutes.java
@@ -1,4 +1,19 @@
 package verifyuser_use_case;
 
+import java.util.Date;
+
+
+/**
+ * @param minutes: time to add
+ * @param beforeTime: previous time
+ * @return afterAddingMins: new time
+ */
+
 public class AddMinutes {
+    public static Date addMinutesToDate(int minutes, Date beforeTime) {
+        long curTimeInMs = beforeTime.getTime();
+        Date afterAddingMins = new Date(curTimeInMs
+                + (minutes * 60000));
+        return afterAddingMins;
+    }
 }
--- src/main/java/verifyuser_use_case/SendEmail.java
+++ src/main/java/verifyuser_use_case/SendEmail.java
@@ -1,4 +1,9 @@
 package verifyuser_use_case;
 
+import java.util.*;
+import javax.mail.*
+import javax.mail.internet.*;
+
+
 public class SendEmail {
 }
