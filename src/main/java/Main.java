855f00111a81bc48861b141da2a9b764b517f4c7
David Lin <davidzy.lin@mail.utoronto.ca>
2022-11-19 15:00:39 -0500

    Add LoginPresenter

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,12 +1,34 @@
-import DataModels.*;
-import Interactors.UserGetOrderInteractor;
-import org.bson.types.ObjectId;
+import Presenters.*;
+
+import javax.swing.*;
+import java.awt.*;
+import java.io.IOException;
 
 public class Main {
-
     public static void main(String[] args) {
-        UserGetOrderInteractor userGetOrderInteractor = new UserGetOrderInteractor();
 
-        userGetOrderInteractor.getOrders(new UserGetOrderRequestModel(new ObjectId("63335e7abb6cd6599ed6f64b"))).getOrders().forEach(item -> System.out.println(item.getOrderStatus()));
+        // Build the main program window
+        JFrame application = new JFrame("Login Example");
+        CardLayout cardLayout = new CardLayout();
+        JPanel screens = new JPanel(cardLayout);
+        application.add(screens);
+
+
+        // Build the GUI, plugging in the parts
+        LoginPresenter loginPresenter = new LoginPresenter();
+        screens.add(loginPresenter.getContentPane(), "login");
+        cardLayout.show(screens, "login");
+        application.pack();
+        application.setVisible(true);
+
+        // Unused screens; we'll uncomment this later
+//        WelcomeScreen welcomeScreen = new WelcomeScreen();
+//        LoginScreen loginScreen = new LoginScreen();
+//        LoggedInScreen loggedInScreen = new LoggedInScreen();
+//        screens.add(welcomeScreen, "register");
+//        screens.add(loginScreen, "login");
+//        screens.add(loggedInScreen, "loggedIn");
+
     }
+
 }
