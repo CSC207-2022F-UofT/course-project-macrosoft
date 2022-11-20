a5dea88b8a57318aa0553759d77a4e7554ade54d
leoyliu <liuyuanyi1208@gmail.com>
2022-11-19 19:03:54 -0500

    implemented user input parameter

--- src/main/java/SearchingAlgorithmUseCase/GetNameFromUser.java
+++ src/main/java/SearchingAlgorithmUseCase/GetNameFromUser.java
@@ -1,1 +1,17 @@
+package SearchingAlgorithmUseCase;
 
+import java.util.Scanner;
+
+public class GetNameFromUser {
+    public static void main(String[] args) {
+        Scanner myObj = new Scanner(System.in);
+
+        System.out.println("Enter restaurant name:");
+
+        // String input
+        String name = myObj.nextLine();
+
+        // Output input by user
+        System.out.println("Name: " + SearchRestaurants.getRestaurant(name).getName());
+    }
+}
