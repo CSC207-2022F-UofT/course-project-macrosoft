1cddfc8e45d1eea15b4d3414936f63737f08b7f1
KaiwenYangUT <kaiwen.yang@mail.utoronto.ca>
2022-11-26 21:35:29 -0500

    Added line by line comments in UpdateOrderStatusInteractor class.

--- src/main/java/UpdateOrderUseCase/UpdateOrderStatusInteractor.java
+++ src/main/java/UpdateOrderUseCase/UpdateOrderStatusInteractor.java
@@ -12,13 +12,24 @@ import org.bson.conversions.Bson;
 public class UpdateOrderStatusInteractor {
 
     private static DBConnection connectionManager = new MongoConnection();
+    // creating a DB connection object
 
     public void updateOrderStatus(ObjectId orderId, String orderStatus) {
+        // taking two parameter orderId and orderStatus.
+        // orderId is an ObjectId, used for tracking the specific order that we want to update the status of it.
+        // orderStatus is a String, used for updating the order status of an order
 
         Bson idFilter = Filters.eq("_id", orderId);
+        // filter out the specific Order with order id ("_id" in DB) orderID
+
         Bson updates = Updates.set("orderStatus", orderStatus);
+        // set the order status ("orderStatus" in DB) of the order to orderStatus.
 
         connectionManager.getCollection("Orders")
+                // get the order collection that are named "Orders" in our DB
+
                 .updateOne(idFilter, updates);
+        // using updateOne to update the order that filtered by idFilter and update its order status with updates.
+
     }
 }
