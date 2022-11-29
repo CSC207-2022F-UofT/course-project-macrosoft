0e1b8bf26a4080bd480f068cf971aea5697bad2d
KaiwenYangUT <kaiwen.yang@mail.utoronto.ca>
2022-11-29 02:53:09 -0500

    Implemented UpdateOrderStatusResponseModel, UpdateOrderStatusInteractor, UpdateOrderStatusController, UpdateOrderStatusHelper, UpdateOrderStatusInputBoundary, UpdateOrderStatusPresenter, UpdateOrderStatusRequestModel

--- src/main/java/UpdateOrderStatusUseCase/UpdateOrderStatusRequestModel.java
+++ src/main/java/UpdateOrderStatusUseCase/UpdateOrderStatusRequestModel.java
@@ -1,5 +1,35 @@
 package UpdateOrderStatusUseCase;
 
+// Application Business Rule Layer
+
+import Entities.*;
+
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+
+import java.util.Date;
+import java.util.List;
+
 public class UpdateOrderStatusRequestModel {
+
+    private Order curOrder;
+    private String newStatus;
+
+    public UpdateOrderStatusRequestModel(Order curOrder, String newStatus) {
+        this.curOrder = curOrder;
+        this.newStatus = newStatus;
+    }
+
+    public Order getCurOrder() {return curOrder;}
+
+    public String getNewStatus() {return newStatus;}
+
+
+
+
+
+
+
+
 
 }
