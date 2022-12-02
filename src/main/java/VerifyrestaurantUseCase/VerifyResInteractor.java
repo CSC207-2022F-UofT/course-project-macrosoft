4212996ffd31ac0d84389dd824761f99fafe60fc
allenxu <allenx.03819@gmail.com>
2022-12-02 16:40:05 -0500

    Implemented VerifyResInteractor, VerifyResPresenter, and VerifyResProcessor.

---
+++ src/main/java/VerifyrestaurantUseCase/VerifyResInteractor.java
@@ -0,0 +1,38 @@
+package VerifyrestaurantUseCase;
+
+import Database.*;
+import VerifyrestaurantUseCase.GetCodeInteractor;
+import VerifyrestaurantUseCase.VerifyResPresenter;
+import org.bson.types.ObjectId;
+
+public class VerifyResInteractor {
+    private VerifyResPresenter presenter;
+    /**
+     * @param restaurantId
+     * @param code
+     * @return 1000: Success
+     * 1001: Entered Incorrect Code
+     * 1002: Expired or code doesn't exist
+     */
+    public int verifyRes(ObjectId restaurantId, String code) {
+        MongoCollectionFetcher fetcher = new MongoCollectionFetcher();
+        VerificationCodeDataGateway verificationCodeDataGateway = new VerificationCodeProcessorMongo(fetcher);
+        UserDataGateway userDataGateway = new UserDataProcessorMongo(fetcher);
+
+        String actualCode = GetCodeInteractor.getVerificationCode(restaurantId);
+
+        if (actualCode.isEmpty()) {
+            presenter.verifiedFailed();
+            return 1002;
+        } else if (!code.equals(actualCode)) {
+            presenter.verifiedFailed();
+            return 1001;
+        }
+
+        verificationCodeDataGateway.deleteByUserId(restaurantId);
+        userDataGateway.updateVerifiedStatus(restaurantId, true);
+
+        presenter.verifiedSuccess(restaurantId);
+        return 1000;
+    }
+}
