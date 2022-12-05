59b09a1994207e12d2cf5a63aae1cb72737cdfdd
David Lin <davidzy.lin@mail.utoronto.ca>
2022-12-05 18:17:20 -0500

    Implemented register user

---
+++ src/main/java/register_use_case/RegisterUserProcessor.java
@@ -0,0 +1,26 @@
+package register_use_case;
+
+public class RegisterUserProcessor implements RegisterUserPresenter {
+
+    RegisterUserScreenInterface screen;
+
+    public RegisterUserProcessor(RegisterUserScreenInterface screen) {
+        this.screen = screen;
+    }
+
+    @Override
+    public void registerFailed(String message) {
+        screen.showMessage(message);
+    }
+
+    @Override
+    public void registerSuccessful(String message) {
+        screen.showMessage(message);
+        screen.showWelcomePage();
+        screen.close();
+    }
+
+    public void setScreen(RegisterUserScreenInterface screen) {
+        this.screen = screen;
+    }
+}
