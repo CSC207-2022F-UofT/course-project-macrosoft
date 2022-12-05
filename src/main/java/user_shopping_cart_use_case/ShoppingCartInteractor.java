81058587963516b685a361c293dc7ce7564a7eaf
inorrr <inor.zhao@mail.utoronto.ca>
2022-12-05 17:29:20 -0500

    Implemented the structure for the shopping cart use case.

--- src/main/java/user_shopping_cart_use_case/ShoppingCartInteractor.java
+++ src/main/java/user_shopping_cart_use_case/ShoppingCartInteractor.java
@@ -1,12 +1,4 @@
 package user_shopping_cart_use_case;
 
-import entities.Food;
-import org.bson.types.ObjectId;
-
-public class ShoppingCartInteractor implements ShoppingCartInputBoundary{
-
-    @Override
-    public void addToCart(ShoppingCartSingleton shoppingCartSingleton, ObjectId foodId, int quantity) {
-        shoppingCartSingleton.getCart().put(foodId, quantity);
-    }
+public class ShoppingCartInteractor {
 }
