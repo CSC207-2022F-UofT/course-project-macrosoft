1eddfe88e49ebd28d35c42fc7ebb586f439fdf90
inorrr <inor.zhao@mail.utoronto.ca>
2022-12-05 22:02:11 -0500

    Updated the Ui design of the homepage.

--- src/main/java/screens/RestaurantComponent.java
+++ src/main/java/screens/RestaurantComponent.java
@@ -6,12 +6,24 @@ import user_shopping_cart_use_case.ShoppingCartSingleton;
 import user_view_menu_use_case.*;
 
 import javax.swing.*;
+import javax.swing.border.Border;
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.util.HashMap;
 
 public class RestaurantComponent extends JPanel {
+
+    private static final Color BG_DARK_GREEN =  new Color(38, 73, 65);
+    private static final Color BG_LIGHT_GREEN = new Color(87, 118, 83);
+    private static final Color HL_LIGHT_GREEN = new Color(166, 199, 148);
+    private static final Color HL_ORANGE_YELLOW = new Color(232, 181, 93);
+    private static final Color GREY_WHITE = new Color(214, 210, 205);
+    private static final Color WHITE = new Color(255, 255, 255);
+
+    private static final Border emptyBorder = BorderFactory.createEmptyBorder(30, 30, 30, 30);
+    private static final Border emptyBorder2 = BorderFactory.createEmptyBorder(0, 10, 0, 10);
+    private static final Border blackline = BorderFactory.createLineBorder(Color.black);
 
     public RestaurantComponent(JLabel label, ObjectId restaurantId) {
         JButton detailButton = new JButton("Detail");
@@ -42,5 +54,7 @@ public class RestaurantComponent extends JPanel {
 
         this.add(label);
         this.add(detailButton);
+        this.setOpaque(true);
+        this.setBackground(GREY_WHITE);
     }
 }
--- src/main/java/user_display_restaurants_use_case/UserDisplayRestaurantPanel.java
+++ src/main/java/user_display_restaurants_use_case/UserDisplayRestaurantPanel.java
@@ -5,6 +5,7 @@ import org.springframework.data.mongodb.core.messaging.Message;
 import screens.RestaurantComponent;
 
 import javax.swing.*;
+import javax.swing.border.Border;
 import java.awt.*;
 import java.util.ArrayList;
 import java.util.Dictionary;
@@ -13,14 +14,28 @@ import java.util.HashMap;
 public class UserDisplayRestaurantPanel extends JPanel implements UserDisplayRestaurantPanelInterface {
 
     UserDisplayRestaurantController controller;
+
     JPanel restaurantPanel = new JPanel(new GridLayout(0, 1));
+
+    private static final Color BG_DARK_GREEN =  new Color(38, 73, 65);
+    private static final Color BG_LIGHT_GREEN = new Color(87, 118, 83);
+    private static final Color HL_LIGHT_GREEN = new Color(166, 199, 148);
+    private static final Color HL_ORANGE_YELLOW = new Color(232, 181, 93);
+    private static final Color GREY_WHITE = new Color(214, 210, 205);
+    private static final Color WHITE = new Color(255, 255, 255);
+
+    private static final Border emptyBorder = BorderFactory.createEmptyBorder(30, 30, 30, 30);
+    private static final Border emptyBorder2 = BorderFactory.createEmptyBorder(0, 10, 0, 10);
+    private static final Border blackline = BorderFactory.createLineBorder(Color.black);
 
     public UserDisplayRestaurantPanel(UserDisplayRestaurantController controller) {
         this.controller = controller;
 
         this.setLayout(new GridLayout(0, 1));
+        restaurantPanel.setOpaque(true);
+        restaurantPanel.setBackground(BG_DARK_GREEN);
 
-        this.add(restaurantPanel);
+//        this.add(restaurantPanel);
     }
 
     public void refreshData() {
@@ -29,11 +44,22 @@ public class UserDisplayRestaurantPanel extends JPanel implements UserDisplayRes
 
     public void updateRestaurantPanel(HashMap<ObjectId, String> restaurantInfo) {
         restaurantPanel = new JPanel(new GridLayout(0, 1));
+        restaurantPanel.setOpaque(true);
+        restaurantPanel.setBackground(BG_DARK_GREEN);
+        restaurantPanel.setBorder(emptyBorder);
 
         for (ObjectId restId : restaurantInfo.keySet()) {
+            JLabel restLabel = new JLabel(restaurantInfo.get(restId));
+            restLabel.setForeground(BG_DARK_GREEN);
+            restLabel.setFont(new Font("Serif", Font.PLAIN, 15));
             restaurantPanel.add(new RestaurantComponent(new JLabel(restaurantInfo.get(restId)), restId));
         }
 
-        this.add(restaurantPanel);
+
+        JScrollPane resScrollPanel = new JScrollPane(restaurantPanel);
+        resScrollPanel.setOpaque(true);
+        restaurantPanel.setBackground(BG_DARK_GREEN);
+        resScrollPanel.setVerticalScrollBarPolicy(ScrollPaneConstants.VERTICAL_SCROLLBAR_ALWAYS);
+        this.add(resScrollPanel);
     }
 }
