5a67437528ad651909c6ad4b6260f8ee3b8228c5
inorrr <inor.zhao@mail.utoronto.ca>
2022-12-06 01:19:22 -0500

    Update the ChangePasswordScreen, ChangeRestaurantInfoScreen, ChangeUserInfoScreen.

--- src/main/java/change_password_use_case/ChangePasswordScreen.java
+++ src/main/java/change_password_use_case/ChangePasswordScreen.java
@@ -3,6 +3,7 @@ package change_password_use_case;
 import screens.LabelTextPanel;
 
 import javax.swing.*;
+import javax.swing.border.Border;
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
@@ -12,6 +13,18 @@ import static javax.swing.JOptionPane.showMessageDialog;
 // Frameworks/Drivers layer
 
 public class ChangePasswordScreen extends JFrame implements ChangePasswordScreenInterface, ActionListener {
+
+    private static final Color BG_DARK_GREEN =  new Color(38, 73, 65);
+    private static final Color BG_LIGHT_GREEN = new Color(87, 118, 83);
+    private static final Color HL_LIGHT_GREEN = new Color(166, 199, 148);
+    private static final Color HL_ORANGE_YELLOW = new Color(232, 181, 93);
+    private static final Color GREY_WHITE = new Color(214, 210, 205);
+    private static final Color WHITE = new Color(255, 255, 255);
+
+    private static final Border emptyBorder = BorderFactory.createEmptyBorder(100, 30, 30, 30);
+    private static final Border emptyBorder2 = BorderFactory.createEmptyBorder(0, 10, 0, 10);
+    private static final Border blackline = BorderFactory.createLineBorder(Color.black);
+
     /**
      * The username chosen by the user
      */
@@ -30,15 +43,43 @@ public class ChangePasswordScreen extends JFrame implements ChangePasswordScreen
     public ChangePasswordScreen(ChangePasswordController controller) {
         this.changePasswordController = controller;
 
-        JLabel title = new JLabel("Change Password");
-        title.setAlignmentX(Component.CENTER_ALIGNMENT);
+        this.setSize(900, 700);
+        this.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+        this.setLocationRelativeTo(null);
+        this.getContentPane().setBackground(GREY_WHITE);
 
-        LabelTextPanel usernameInfo = new LabelTextPanel(
-                new JLabel("Original Password"), originalPassword);
-        LabelTextPanel passwordInfo = new LabelTextPanel(
-                new JLabel("New Password"), newPassword);
-        LabelTextPanel confirmPasswordInfo = new LabelTextPanel(
-                new JLabel("Confirm New Password"), confirmNewPassword);
+        JPanel titlePanel = new JPanel();
+        titlePanel.setOpaque(true);
+        titlePanel.setBackground(GREY_WHITE);
+        titlePanel.setBorder(emptyBorder);
+        JLabel title = new JLabel("Change Password");
+        title.setFont(new Font("Serif", Font.BOLD|Font.ITALIC, 40));
+        title.setForeground(BG_DARK_GREEN);
+        title.setAlignmentX(Component.CENTER_ALIGNMENT);
+        titlePanel.add(title);
+
+        JLabel username = new JLabel("Original Password");
+        username.setFont(new Font("Serif", Font.PLAIN, 15));
+        username.setForeground(BG_DARK_GREEN);
+
+        JLabel password = new JLabel("New Password");
+        password.setFont(new Font("Serif", Font.PLAIN, 15));
+        password.setForeground(BG_DARK_GREEN);
+
+        JLabel confirm = new JLabel("Confirm New Password");
+        confirm.setFont(new Font("Serif", Font.PLAIN, 15));
+        confirm.setForeground(BG_DARK_GREEN);
+
+        LabelTextPanel usernameInfo = new LabelTextPanel(username, originalPassword);
+        LabelTextPanel passwordInfo = new LabelTextPanel(password, newPassword);
+        LabelTextPanel confirmPasswordInfo = new LabelTextPanel(confirm, confirmNewPassword);
+
+        usernameInfo.setOpaque(true);
+        passwordInfo.setOpaque(true);
+        confirmPasswordInfo.setOpaque(true);
+        usernameInfo.setBackground(GREY_WHITE);
+        passwordInfo.setBackground(GREY_WHITE);
+        confirmPasswordInfo.setBackground(GREY_WHITE);
 
         JButton save = new JButton("Save");
         JButton cancel = new JButton("Cancel");
@@ -46,21 +87,28 @@ public class ChangePasswordScreen extends JFrame implements ChangePasswordScreen
         JPanel buttons = new JPanel();
         buttons.add(save);
         buttons.add(cancel);
+        buttons.setOpaque(true);
+        buttons.setBackground(GREY_WHITE);
 
         save.addActionListener(this);
         cancel.addActionListener(this);
 
         JPanel main = new JPanel();
         main.setLayout(new BoxLayout(main, BoxLayout.Y_AXIS));
+        main.setOpaque(true);
+        main.setBackground(GREY_WHITE);
 
-        main.add(title);
-        main.add(usernameInfo);
-        main.add(passwordInfo);
-        main.add(confirmPasswordInfo);
+        JPanel infoPanel = new JPanel();
+        infoPanel.setLayout(new GridLayout(5, 1));
+        infoPanel.add(usernameInfo);
+        infoPanel.add(passwordInfo);
+        infoPanel.add(confirmPasswordInfo);
+        infoPanel.setOpaque(false);
+
+        main.add(titlePanel);
+        main.add(infoPanel);
         main.add(buttons);
         this.setContentPane(main);
-
-        this.pack();
     }
 
     /**
