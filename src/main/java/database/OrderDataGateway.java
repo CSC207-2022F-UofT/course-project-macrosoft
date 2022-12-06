30994f096d929589788a87c3d547e2969116bfaa
KaiwenYangUT <kaiwen.yang@mail.utoronto.ca>
2022-12-05 20:31:04 -0500

    Added updateOrderStatus method in OrderDataGateway interface.

--- src/main/java/database/OrderDataGateway.java
+++ src/main/java/database/OrderDataGateway.java
@@ -8,10 +8,11 @@ import java.util.List;
 public interface OrderDataGateway {
     String save(Order order);
     String delete(Order order);
-    ObjectId create(Order order);
+    String create(Order order);
     List<Order> findAll();
     List<Order> findAllByUser(ObjectId userId);
     List<Order> findAllByRestaurant(ObjectId restaurantId);
     Order findById(ObjectId id);
-    void updateStatus(ObjectId orderId, String newStatus);
+
+    void updateOrderStatus(ObjectId orderId, String newStatus);
 }
