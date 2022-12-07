c0230673d3867690180ef062e641e7fb6d6da5c3
KaiwenYangUT <kaiwen.yang@mail.utoronto.ca>
2022-12-07 03:27:03 -0500

    Implemented a test case for UpdateOrderStatusInteractorTest in test_update_order_status_use_case package, and passed.

--- src/test/java/test_update_order_status_use_case/UpdateOrderStatusInteractorTest.java
+++ src/test/java/test_update_order_status_use_case/UpdateOrderStatusInteractorTest.java
@@ -10,6 +10,7 @@ import org.junit.Test;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import update_order_status_use_case.*;
+import user_verify_use_case.GetCodeInteractor;
 
 import java.util.ArrayList;
 import java.util.Date;
@@ -26,29 +27,26 @@ public class UpdateOrderStatusInteractorTest {
         Order order = TestFactory.getOrder();
     }
 
-//    @Test
-//    void testUpdateOrderStatus() {
-//        ObjectId orderId = new ObjectId("63335f66bb6cd6599ed6f64d");
-//        String newStatus = new String("Order Cancelled");
-//        String actual = new String("Order Received");
-//        UpdateOrderStatusRequestModel request = new UpdateOrderStatusRequestModel(orderId, newStatus);
-//        UpdateOrderStatusController controller = new UpdateOrderStatusController(interactor, orderId);
-//        UpdateOrderStatusScreenInterface screen = new UpdateOrderStatusScreen(controller);
-//        UpdateOrderStatusPresenter presenter = new UpdateOrderStatusProcessor(screen);
-//        UpdateOrderStatusInteractor interactor = new UpdateOrderStatusInteractor(presenter);
-//
-//        assertEquals(.cccc.ccccc., actual);
-//        // assertTrue(update_order_status_use_case.UpdateOrderStatusInteractor
-//
-//    }
-
-
-    // delete the created order from database after the test have been run
-    @AfterEach
-    void tearDown() {
+    @Test
+    public void testUpdateOrderStatus() {
         ObjectId orderId = new ObjectId("63335f66bb6cd6599ed6f64d");
-        MongoCollectionFetcher fetcher = new MongoCollectionFetcher();
-        OrderDataGateway gateway = new OrderDataProcessorMongo(fetcher);
-        gateway.delete(gateway.findById(orderId));
+        String newStatus = "Order Cancelled";
+
+        UpdateOrderStatusRequestModel request = new UpdateOrderStatusRequestModel(orderId, newStatus);
+        UpdateOrderStatusPresenter presenter = new UpdateOrderStatusProcessor(null);
+        UpdateOrderStatusInteractor interactor = new UpdateOrderStatusInteractor(presenter);
+        String expected = "Order Cancelled";
+        interactor.updateOrderStatus(request);
+        assertEquals(interactor.getOrderStatus(orderId), expected);
     }
+
+     // delete the created order from database after the test have been run
+     @AfterEach
+     void tearDown() {
+         ObjectId orderId = new ObjectId("63335f66bb6cd6599ed6f64d");
+         MongoCollectionFetcher fetcher = new MongoCollectionFetcher();
+         OrderDataGateway gateway = new OrderDataProcessorMongo(fetcher);
+         gateway.delete(gateway.findById(orderId));
+     }
+
 }
