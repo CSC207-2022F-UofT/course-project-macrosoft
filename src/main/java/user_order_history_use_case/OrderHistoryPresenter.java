4dce624773802f3bcac99b8e307c8b9bf80da2d0
inorrr <inor.zhao@mail.utoronto.ca>
2022-12-07 21:36:36 -0500

    Addressed all warnings.

--- src/main/java/user_order_history_use_case/OrderHistoryPresenter.java
+++ src/main/java/user_order_history_use_case/OrderHistoryPresenter.java
@@ -1,5 +1,7 @@
 package user_order_history_use_case;
-
+/*
+presenter of the use case
+ */
 public interface OrderHistoryPresenter {
     void orderFound(OrderHistoryResponseModel orderHistoryResponseModel);
     void orderNotFound();
