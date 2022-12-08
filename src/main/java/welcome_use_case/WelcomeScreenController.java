a0ed3349f59f14b38a88c71c39bf7e232f079dc1
inorrr <inor.zhao@mail.utoronto.ca>
2022-12-07 21:59:06 -0500

    Addressed all warnings. Implemented the welcome screen controller.

--- src/main/java/welcome_use_case/WelcomeScreenController.java
+++ src/main/java/welcome_use_case/WelcomeScreenController.java
@@ -1,5 +1,21 @@
 package welcome_use_case;
 
+import components.ScreenFactory;
+
 public class WelcomeScreenController {
 
+    public void getLoginScreen(){
+        ScreenFactory screenFactory = new ScreenFactory();
+        screenFactory.createLoginSreen();
+    }
+
+    public void getRegisterRestaurantScreen(){
+        ScreenFactory screenFactory = new ScreenFactory();
+        screenFactory.createRestaurantRegisterScreen();
+    }
+
+    public void getRegisterUserScreen(){
+        ScreenFactory screenFactory = new ScreenFactory();
+        screenFactory.createUserRegisterScreen();
+    }
 }
