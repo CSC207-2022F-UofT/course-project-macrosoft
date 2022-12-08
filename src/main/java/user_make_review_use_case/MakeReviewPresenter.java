f4486f3e31ca446e8e76ab6d6268e5b4bef62508
inorrr <inor.zhao@mail.utoronto.ca>
2022-12-07 21:33:52 -0500

    Addressed all warnings.

--- src/main/java/user_make_review_use_case/MakeReviewPresenter.java
+++ src/main/java/user_make_review_use_case/MakeReviewPresenter.java
@@ -1,5 +1,9 @@
 package user_make_review_use_case;
 
+/**
+ * presenter of the make review use case.
+ * prepares the success and fail view
+ */
 public interface MakeReviewPresenter {
 
     MakeReviewResponseModel prepareSuccessView(MakeReviewResponseModel responseModel);
