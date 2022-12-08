f463b57564ebaae259f9ca23daf9313a6f8ec8e1
jacksonsujh <72714023+jacksonsujh@users.noreply.github.com>
2022-12-07 20:31:52 -0500

    Added javadoc for the order history controller

--- src/main/java/user_order_history_use_case/OrderHistoryController.java
+++ src/main/java/user_order_history_use_case/OrderHistoryController.java
@@ -2,15 +2,29 @@ package user_order_history_use_case;
 
 import org.bson.types.ObjectId;
 
+
+/**
+ * This class is the controller for the user get order history use case.
+ */
 public class OrderHistoryController {
     private final OrderHistoryInputBoundary orderHistoryInteractor;
     ObjectId currentUserId;
 
+    /**
+     * Constructor for UserLoginController
+     *
+     * @param orderHistoryInteractor       the user order history controller
+     * @param currentUserId the current user id
+     */
     public OrderHistoryController(OrderHistoryInputBoundary orderHistoryInteractor, ObjectId currentUserId) {
         this.orderHistoryInteractor = orderHistoryInteractor;
         this.currentUserId = currentUserId;
     }
 
+
+    /**
+     * Get all user's order history
+     */
     public void getOrders() {
         if(currentUserId == null){
             return;
@@ -21,6 +35,10 @@ public class OrderHistoryController {
         orderHistoryInteractor.displayOrders(requestModel);
     }
 
+
+    /**
+     * Get user ongoing order
+     */
     public void getCurrentOrder() {
         if(currentUserId == null){
             return;
@@ -30,6 +48,10 @@ public class OrderHistoryController {
 
         orderHistoryInteractor.displayCurrentOrders(requestModel);
     }
+
+    /**
+     * Get the current user id
+     */
     public ObjectId getCurrentUserId() {
         return currentUserId;
     }
