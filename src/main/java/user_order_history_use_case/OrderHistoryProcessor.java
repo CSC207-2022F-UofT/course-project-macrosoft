b465c96515bf404a2a37fd36d4441cac0a262ce9
jacksonsujh <72714023+jacksonsujh@users.noreply.github.com>
2022-12-07 23:36:14 -0500

    Added the javadoc for the order history processor

--- src/main/java/user_order_history_use_case/OrderHistoryProcessor.java
+++ src/main/java/user_order_history_use_case/OrderHistoryProcessor.java
@@ -3,20 +3,41 @@ package user_order_history_use_case;
 public class OrderHistoryProcessor implements OrderHistoryPresenter{
     private OrderHistoryPanelInterface orderHistoryPanel;
 
+    /**
+     * A constructor for the order history panel
+     *
+     * @param orderHistoryPanel The order history panel
+     */
     public OrderHistoryProcessor(OrderHistoryPanelInterface orderHistoryPanel) {
         this.orderHistoryPanel = orderHistoryPanel;
     }
 
+
+    /**
+     * Display the orders when the correct orders were found
+     *
+     * @param orderHistoryResponseModel An instance of order history response model
+     */
     @Override
     public void orderFound(OrderHistoryResponseModel orderHistoryResponseModel) {
         orderHistoryPanel.setOrder(orderHistoryResponseModel);
     }
 
+
+    /**
+     * Display when the orders were not correctly found
+     */
     @Override
     public void orderNotFound() {
 
     }
 
+
+    /**
+     * Set the order history panel to the given panel
+     *
+     * @param orderHistoryPanel An order history panel object
+     */
     @Override
     public void setOrderHistoryPanel(OrderHistoryPanelInterface orderHistoryPanel) {
         this.orderHistoryPanel = orderHistoryPanel;
