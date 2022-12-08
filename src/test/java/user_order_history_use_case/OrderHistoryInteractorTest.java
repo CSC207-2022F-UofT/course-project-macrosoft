6b54776f68dd828ca51bac0f5788b800856995af
jacksonsujh <72714023+jacksonsujh@users.noreply.github.com>
2022-12-07 20:13:49 -0500

    Implemented OrderHistoryInteractorTest

---
+++ src/test/java/user_order_history_use_case/OrderHistoryInteractorTest.java
@@ -0,0 +1,13 @@
+package user_order_history_use_case;
+
+import org.junit.jupiter.api.Test;
+
+public class OrderHistoryInteractorTest {
+    @Test
+    void displayOrders() {
+    }
+
+    @Test
+    void displayCurrentOrders() {
+    }
+}
