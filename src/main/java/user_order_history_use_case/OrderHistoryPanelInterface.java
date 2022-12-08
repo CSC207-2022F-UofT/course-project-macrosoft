13cdc90c19746ff468be94f964ce018332babeda
jacksonsujh <72714023+jacksonsujh@users.noreply.github.com>
2022-12-07 21:52:44 -0500

    Added the javadoc for the order history panel interface

--- src/main/java/user_order_history_use_case/OrderHistoryPanelInterface.java
+++ src/main/java/user_order_history_use_case/OrderHistoryPanelInterface.java
@@ -1,7 +1,21 @@
 package user_order_history_use_case;
 
+
+/**
+ * This is the order history panel interface for the order history use case
+ */
 public interface OrderHistoryPanelInterface {
+
+    /**
+     * Get updates of the order history
+     */
     void updateOrder();
 
+
+    /**
+     * set the order panel
+     *
+     * @param orderHistoryResponseModel response model
+     */
     void setOrder(OrderHistoryResponseModel orderHistoryResponseModel);
 }
