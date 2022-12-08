b4b641683cee90389e8da5358b69a807a9f765af
inorrr <inor.zhao@mail.utoronto.ca>
2022-12-07 21:49:15 -0500

    Addressed all warnings.

--- src/main/java/user_verify_use_case/VerifyUserPresenter.java
+++ src/main/java/user_verify_use_case/VerifyUserPresenter.java
@@ -1,9 +1,11 @@
 package user_verify_use_case;
-
+/*
+presenter of the use case
+ */
 import org.bson.types.ObjectId;
 
 public interface VerifyUserPresenter {
     void setVerifyUserScreen(VerifyUserScreenInterface verifyUserScreen);
-    String verifiedSuccess(ObjectId currentUser);
-    String verifiedFailed();
+    void verifiedSuccess(ObjectId currentUser);
+    void verifiedFailed();
 }
