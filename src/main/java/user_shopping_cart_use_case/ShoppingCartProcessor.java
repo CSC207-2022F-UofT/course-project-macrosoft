f01bb55bc74b9ffbc47040984efc456db01c4e10
inorrr <inor.zhao@mail.utoronto.ca>
2022-12-07 21:45:57 -0500

    Addressed all warnings.

--- src/main/java/user_shopping_cart_use_case/ShoppingCartProcessor.java
+++ src/main/java/user_shopping_cart_use_case/ShoppingCartProcessor.java
@@ -10,11 +10,20 @@ public class ShoppingCartProcessor implements ShoppingCartPresenter {
         this.screen = screen;
     }
 
+    /**
+     * display the shopping cart screen with given information
+     * @param restaurantName name of the current restaurant
+     * @param shoppingCartDisplay items in the shopping cart
+     */
     @Override
     public void displayShoppingCart(String restaurantName, HashMap<String, HashMap<String, Object>> shoppingCartDisplay) {
         screen.displayShoppingCart(restaurantName, shoppingCartDisplay);
     }
 
+    /**
+     * set the screen to the given screen
+     * @param screen new screen
+     */
     public void setScreen(ShoppingCartPanelInterface screen) {
         this.screen = screen;
     }
