c502a18785f2c634e9e40caa918735b15bf3b830
jacksonsujh <72714023+jacksonsujh@users.noreply.github.com>
2022-12-07 23:46:31 -0500

    Added the javadoc for the change restaurant info presenter

--- src/main/java/change_restaurant_info_use_case/ChangeRestaurantInfoPresenter.java
+++ src/main/java/change_restaurant_info_use_case/ChangeRestaurantInfoPresenter.java
@@ -1,10 +1,30 @@
 package change_restaurant_info_use_case;
 
 
+/**
+ * This interface is for the change restaurant info presenter in change restaurant info use case
+ */
 public interface ChangeRestaurantInfoPresenter {
+
+    /**
+     * Show change the restaurant information succeeded screen
+     *
+     * @param message the message to be shown
+     */
     void changeRestaurantInfoSuccess(String message);
 
+    /**
+     * Show change the restaurant information failed screen
+     *
+     * @param message the message to be shown
+     */
     void changeRestaurantInfoFailed(String message);
 
+
+    /**
+     * Set the screen to the screen given
+     *
+     * @param screen the change restaurant info screen
+     */
     void setScreen(ChangeRestaurantInfoScreenInterface screen);
 }
