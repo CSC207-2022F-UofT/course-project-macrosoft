bf572be74c9b80d45940358acaf650118b5c7208
jacksonsujh <72714023+jacksonsujh@users.noreply.github.com>
2022-12-07 23:51:59 -0500

    Added more javadoc for the change restaurant info processor

--- src/main/java/change_restaurant_info_use_case/ChangeRestaurantInfoProcessor.java
+++ src/main/java/change_restaurant_info_use_case/ChangeRestaurantInfoProcessor.java
@@ -1,18 +1,25 @@
 package change_restaurant_info_use_case;
 
 /**
- * This class is the processor for the change restaurant informatoin use case.
+ * This class is the processor for the change restaurant information use case.
  */
 public class ChangeRestaurantInfoProcessor implements ChangeRestaurantInfoPresenter {
     ChangeRestaurantInfoScreenInterface screen;
 
 
+    /**
+     * The constructor for the change restaurant info processor
+     *
+     * @param screen the change restaurant info screen
+     */
     public ChangeRestaurantInfoProcessor(ChangeRestaurantInfoScreenInterface screen) {
         this.screen = screen;
     }
 
     /**
-     * @param message
+     * Show change the restaurant information succeeded screen
+     *
+     * @param message the message to be shown
      */
     @Override
     public void changeRestaurantInfoSuccess(String message) {
@@ -21,17 +28,31 @@ public class ChangeRestaurantInfoProcessor implements ChangeRestaurantInfoPresen
     }
 
     /**
-     * @param message
+     * Show change the restaurant information failed screen
+     *
+     * @param message the message to be shown
      */
     @Override
     public void changeRestaurantInfoFailed(String message) {
         this.screen.showMessage(message);
     }
 
+
+    /**
+     * Get the change restaurant info screen
+     *
+     * @return the change restaurant info screen
+     */
     public ChangeRestaurantInfoScreenInterface getScreen() {
         return screen;
     }
 
+
+    /**
+     * Set the change restaurant info screen to the given screen
+     *
+     * @param screen the change restaurant info screen
+     */
     public void setScreen(ChangeRestaurantInfoScreenInterface screen) {
         this.screen = screen;
     }
